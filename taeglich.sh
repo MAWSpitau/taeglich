4309b44edde4512bd24104a801c5cf28612ee288
Marcel Spitau <blog@spitau.de>
2020-12-18 08:17:23 +0100

    es wird eine überschrift für den Tag eingebunden, wenn noch nicht vorhanden

--- taeglich.sh
+++ taeglich.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
+
+# This script is influenced ba the idea of Nick Janetakis.
+# https://github.com/nickjj/notes
+# But it works in a complete other way. :)
+
+# Hier auskommentieren für debugging.
+# DEBUG="=====>>>- DEBUGGING:"
+# USAGE:
+# [[ $DEBUG ]] && echo ${DEBUG} "Nachricht"
+
+
+
 set -e
 
-readonly NOTES_DIRECTORY="${NOTES_DIRECTORY:-"${HOME}/data/wiki/taeglich/$(date +%Y)"}"
-readonly NOTES_EDITOR="${EDITOR}"
+# In welchem Verzeichnis sollen die Einträge gespeichert werden?
+readonly NOTES_DIRECTORY="${HOME}/data/wiki/taeglich/$(date +%Y)" 
 
+# Wie sollen die Dateien heißen? 
 readonly NOTES_FILE="$(date +%Y-%m).md"
+
+# Das ist der Pfad inkl. Dateinamen für die gesamte Datei. 
 readonly NOTES_PATH="${NOTES_DIRECTORY}/${NOTES_FILE}"
 
+# Das Verzeichnis wird für ein neues Jahr angelegt. 
+# Könnte auch automatisch laufen, da es nur einmal im Jahr läuft.
 if [ ! -d "${NOTES_DIRECTORY}" ]; then
     while true; do
         read -rp "${NOTES_DIRECTORY} does not exist, do you want to create it? (y/n) " yn
@@ -18,4 +35,15 @@ if [ ! -d "${NOTES_DIRECTORY}" ]; then
     done
 fi
 
+# Überschrift in Markdown der ersten bene definieren.
+HEADER="# $(date +%Y-%m-%d)"
+[[ $DEBUG ]] && echo ${DEBUG} "HEADER: " ${HEADER}
+
+# Wenn der Header für heute noch nicht gestzt ist, dann erledige ich das für dich und starte mit einem Spiegelstrich
+[[ `grep "${HEADER}" ${NOTES_PATH}` == ""  ]] && printf "\n\n\n${HEADER}\n\n- " >> "${NOTES_PATH}"
+
+# vim startet mit einigen parametern.
+# "+ normal G$" 	- startet im normalen Modus und sprint ans Dateiende und an die letzte Stelle der Zeile
+# + set nofoldenable "  - entfaltet alles. 
+# +startinsert! 	- startet im append Modus. 
 vim "+ normal G$" "+ set nofoldenable " +startinsert! $NOTES_PATH
